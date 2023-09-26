b9d86b85cedca802bef4ce4f373cd46fb50c0022
baharRi <bahar.riazi@hotmail.no>
2023-09-26 23:53:37 +0200

    Fixed nav bar covering content

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const dynamicText = document.querySelector("h1 span");
 const myName = "Bahar Riazi."
 let charIndex = 0; 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior : "smooth"
+        });
+    });
+});
+
 const typeEffect = () => {
     const currentChar = myName.substring(0, charIndex);
     dynamicText.textContent = currentChar;
