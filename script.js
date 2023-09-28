afe666586b67f54c88ec20bb0e107e3a440d4016
baharRi <bahar.riazi@hotmail.no>
2023-09-28 20:25:21 +0200

    Added pages for all projects

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dynamicText = document.querySelector("h1 span");
-const myName = "Bahar Riazi."
+const currentUrl = window.location.href;
+let typingText = "Bahar Riazi."
 let charIndex = 0; 
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -12,16 +13,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 const typeEffect = () => {
-    const currentChar = myName.substring(0, charIndex);
+    const currentChar = typingText.substring(0, charIndex);
     dynamicText.textContent = currentChar;
     dynamicText.classList.add("stopBlinking")
 
-    if (charIndex < myName.length) {
+    if (charIndex < typingText.length) {
         charIndex++;
         setTimeout(typeEffect, 150);
     } else {
         dynamicText.classList.remove("stopBlinking")
     }
 }
+
+if (currentUrl.includes("projectAtomIQ.html")) {
+    typingText = "AtomIQ interaction:"
+} else if (currentUrl.includes("index.html")) {
+    typingText ="Bahar Riazi."
+} else if (currentUrl.includes("projectSvoosh.html")) {
+    typingText ="Svoosh:"
+} else if (currentUrl.includes("projectPAL.html")) {
+    typingText ="P4L:"
+}
+console.log(currentUrl); 
 
 typeEffect();
